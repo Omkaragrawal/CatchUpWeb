fd52a9c2b187b8b1038ed3ac7b8ca3bc4f5ab14a
Omkaragrawal <omkar3654@gmail.com>
2019-02-22 00:06:10 +0530

    temp changes

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const contentHn = document.getElementById('contentHn');
 const postBtn = document.getElementById('posts');
 let count = 1;
 window.onload = param => {
-    if(!window.location.hash){
-    loadData('hackernews');
-    document.location.hash = '#hackernews';
+    if (!window.location.hash) {
+        loadData('hackernews');
+        document.location.hash = '#hackernews';
     } else {
         loadData(window.location.hash.split('#')[1]);
     }
@@ -20,7 +20,9 @@ function subMenu(element) {
 function loadData(channel) {
     switch (channel) {
         case 'hackernews':
-            axios.get('/hackernews', { responseType: 'json' })
+            axios.get('/hackernews', {
+                    responseType: 'json'
+                })
                 .then(response => {
                     postBtn.hidden = true;
                     // console.log(response.data.items);
@@ -31,9 +33,11 @@ function loadData(channel) {
                 })
             break;
         case "hackernewsTop":
-                postBtn.hidden=true;
+            postBtn.hidden = true;
             // console.log("hackernewsTOP");
-            axios.get('https://hacker-news.firebaseio.com/v0/topstories.json', { responseType: 'json' })
+            axios.get('https://hacker-news.firebaseio.com/v0/topstories.json', {
+                    responseType: 'json'
+                })
                 .then(response => {
                     postBtn.hidden = true;
                     // console.log(response.data.items);
@@ -42,10 +46,12 @@ function loadData(channel) {
                 .catch(err => {
                     console.log(err)
                 })
-            postBtn.hidden= false;
+            postBtn.hidden = false;
             break;
         case 'hackerearth':
-            axios.get('/hackerearth', { responseType: 'document' })
+            axios.get('/hackerearth', {
+                    responseType: 'document'
+                })
                 .then(response => {
                     console.log(response.data);
                 })
@@ -63,29 +69,45 @@ function toHnTable(data) {
     contentHn.innerHTML = "";
     for (let i = 0; i < data.length; i++) {
         // console.log(data[i] + "\n")
-        contentHn.innerHTML += '<tr><td colspan="2"><p>' + data[i].content_html.replace(' URL', "").replace(/">.*\s<.*\s.*\s.*/g, `" target="_blank"> ${data[i].title}`) + '</a></div><div>Author: ' + data[i].author + '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspPublished on: ' + data[i].date_published + '</div></p></tdcolspan></tr>';
+        contentHn.innerHTML += '<tr><td colspan="2"><p>' + data[i].content_html.replace(' URL', "").replace(/">.*\s<.*\s.*\s.*/g, `" target="_blank" rel="noopener"> ${data[i].title}`) + '</a></div><div>Author: ' + data[i].author + '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspPublished on: ' + data[i].date_published + '</div></p></tdcolspan></tr>';
     }
-    postBtn.hidden=false;
+    postBtn.hidden = false;
 }
-function toHnlTable (data) {
-    axios.get('https://hacker-news.firebaseio.com/v0/item/' + data[0] + '.json', { responseType: 'json' })
-                .then(response => {
-                    postBtn.hidden = true;
-                    console.log(response.data);
-                    contentHn.hidden=true;
-                    contentHn.innerHTML = '';
-                    contentHn.innerHTML += `<tr><td colspan="2"><p><a href ="${response.data.url}" trget="_blank"> ${response.data.title}</a></div><div>Author:  ${response.data.by} &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspPublished on: ${new Date(response.data.time)} </div></p></tdcolspan></tr>`;
-                    contentHn.hidden=false; 
-                    postBtn.hidden=false;
-                })
-                .catch(err => {
-                    console.log(err)
-                })
+
+function toHnlTable(data) {
+    //console.log(data.length + "\n" + data);
+    let postData = [];
+    for (let i = 0; i < 10; i++) {
+        postBtn.hidden = true;
+        contentHn.innerHTML = '';
+        axios.get('https://hacker-news.firebaseio.com/v0/item/' + data[i] + '.json', {
+                responseType: 'json'
+            })
+            .then(response => {
+                console.log(response.data);
+                postData.push(response.data);
+                contentHn.hidden = true;
+                contentHn.innerHTML += `<tr><td colspan="2"><p><a href ="${response.data.url}" trget="_blank" rel="noopener"> ${response.data.title}</a></div><div>Author:  ${response.data.by} &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspPublished on: ${new Date(response.data.time)} </div></p></tdcolspan></tr>`;
+                contentHn.hidden = false;
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+    console.log(postData)
+    postBtn.hidden = false;
+    // let urlArray = data.map(i => axios.get('https://hacker-news.firebaseio.com/v0/item/' + i + '.json',{ responseType: 'json' }));
+    // console.log(urlArray);
 }
 
 document.getElementById('btnReload').onclick = () => loadData(window.location.hash.replace('#', ''));
 postBtn.onclick = (event) => {
     count++;
-    if (count % 2 == 0) { window.location.hash = "hackernewsTop"; event.srcElement.innerHTML = "Show LATEST"; }
-    else { window.location.hash = "hackernews"; event.srcElement.innerHTML = "Show TOP"; }
+    if (count % 2 == 0) {
+        window.location.hash = "hackernewsTop";
+        event.srcElement.innerHTML = "Show LATEST";
+    } else {
+        window.location.hash = "hackernews";
+        event.srcElement.innerHTML = "Show TOP";
+    }
 }
